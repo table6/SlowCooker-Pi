70dc9dba61ea036ff732a98b5de2c93ac4094740
Mariah Doepker <m2doepker@gmail.com>
2019-03-12 11:25:14 -0400

    Trying new idea for buttons: wiring ground and using the button class.

--- fsm/button_control_fsm.py
+++ fsm/button_control_fsm.py
@@ -2,7 +2,7 @@
 
 from state_machine import state_machine
 from datetime import datetime
-from gpiozero import OutputDevice, Button, InputDevice, LED
+from gpiozero import OutputDevice, Button, Button, LED
 import time
 
 # to fix by Tuesday:
@@ -24,18 +24,18 @@ start_temp = 160 #degrees F
 start_time = 7 #index
 remote_input = "NULL"
 # read ports  inputs to the function
-ON_OFF = InputDevice(4) # pin 7
-PROGRAM_R = InputDevice(27) # pin 13
+ON_OFF = Button(4) # pin 7
+PROGRAM_R = Button(27) # pin 13
 PROGRAM_W = LED(22) # pin 15
-MANUAL_R = InputDevice(18) # pin 12
+MANUAL_R = Button(18) # pin 12
 MANUAL_W = LED(23) # pin 16
-PROBE_R = InputDevice(24) # pin 18
+PROBE_R = Button(24) # pin 18
 PROBE_W = LED(25) # pin 22
-UP_R = InputDevice(5) # pin 29
+UP_R = Button(5) # pin 29
 UP_W = LED(6) # pin 31
-DOWN_R = InputDevice(13) # pin 33
+DOWN_R = Button(13) # pin 33
 DOWN_W = LED(19) # pin 35
-ENTER_R = InputDevice(16) # pin 36
+ENTER_R = Button(16) # pin 36
 ENTER_W = LED(20) # pin 38
 next_state = "on_off_state"
 # input from app won't act like a button, but will be a string
@@ -50,7 +50,7 @@ def on_off_state():
 
 	while next_state == "on_off_state":
 		#control the next state
-		if ON_OFF.value == 1:
+		if ON_OFF.is_pressed:
 			next_state = "sel_state"
 		else:
 			next_state = "on_off_state"
@@ -67,15 +67,15 @@ def sel_state():
 	print("SELECT STATE")
 	while next_state == "sel_state":
 		#control the next state
-		if ON_OFF.value == 1: ##or inactivity_timer.is_met():
+		if ON_OFF.is_pressed: ##or inactivity_timer.is_met():
 			next_state = "on_off_state"
-		elif PROGRAM_R.value == 1:
+		elif PROGRAM_R.is_pressed:
 			user_selection = "program"
 			next_state = "cook_time_state"
-		elif PROBE_R.value == 1:
+		elif PROBE_R.is_pressed:
 			user_selection = "probe"
 			next_state = "heat_setting_state"
-		elif MANUAL_R.value == 1:
+		elif MANUAL_R.is_pressed:
 			user_selection = "manual"
 			next_state = "heat_setting_state"
 		else:
@@ -114,22 +114,22 @@ def cook_time_state():
 	print("COOK TIME STATE")
 	while next_state == "cook_time_state":
 		#control the next state
-		if ON_OFF.value == 1:
+		if ON_OFF.is_pressed:
 			next_state = "on_off_state"
 		##elif inactivity_timer.is_met():
 		##	next_state = "sel_state"
-		elif ENTER_R.value == 1:
+		elif ENTER_R.is_pressed:
 			next_state = "heat_setting_state"
-		elif MANUAL_R.value == 1:
+		elif MANUAL_R.is_pressed:
 			user_selection = "manual"
 			next_state = "heat_setting_state"
-		elif PROBE_R.value == 1:
+		elif PROBE_R.is_pressed:
 			user_selection = "probe"
 			next_state = "heat_setting_state"
-		elif UP_R.value == 1:
+		elif UP_R.is_pressed:
 			if start_time < 23:
 				start_time = start_time + 1		
-		elif DOWN_R.value == 1:
+		elif DOWN_R.is_pressed:
 			if start_time > 0:
 				start_time = start_time - 1
 		else:
@@ -192,20 +192,20 @@ def heat_setting_state():
 	print("HEAT SETTING STATE")
 	while next_state == "heat_setting_state":
 		#control the next state
-		if ON_OFF.value == 1:
+		if ON_OFF.is_pressed:
 			next_state = "on_off_state"
 		##elif inactivity_timer.is_met() and user_selection == "probe":
 		##	next_state = "sel_state"
 		##elif start_timer.is_met() and user_selection != "probe":
 		##	next_state = "display_state"
-		elif ENTER_R.value == 1:
+		elif ENTER_R.is_pressed:
 			if user_selection != "probe":
 				next_state = "display_state"
 			elif heat_selection == "warm":
 				next_state = "display_state"
 			else:
 				next_state = "temp_setting_state"
-		elif PROGRAM_R.value == 1:
+		elif PROGRAM_R.is_pressed:
 			user_selection = "program"
 			next_state = "cook_time_state"
 		else:
@@ -213,13 +213,13 @@ def heat_setting_state():
 		
 		#control the outputs of this state
 		# takes up, down, enter, manual, probe, program
-		if MANUAL_R.value == 1:
+		if MANUAL_R.is_pressed:
 			user_selection = "manual"
 			heat_selection = "high"
-		if PROBE_R.value == 1:
+		if PROBE_R.is_pressed:
 			user_selection = "probe"
 			heat_selection = "high"
-		if UP_R.value == 1:
+		if UP_R.is_pressed:
 		# change heat_selection
 			if heat_selection == "high":
 				heat_selection = "low"
@@ -227,7 +227,7 @@ def heat_setting_state():
 				heat_selection = "warm"
 			else:
 				heat_selection = "high"
-		if DOWN_R.value == 1:
+		if DOWN_R.is_pressed:
 		# change heat_selection
 			if heat_selection == "high":
 				heat_selection = "low"
@@ -278,22 +278,22 @@ def temp_setting_state():
 	print("TEMP SETTING STATE")
 	while next_state == "temp_setting_state":
 		#control the next state
-		if ON_OFF.value == 1:
+		if ON_OFF.is_pressed:
 			next_state = "on_off_state"
-		elif ENTER_R.value == 1: ## or start_timer.is_met():
+		elif ENTER_R.is_pressed: ## or start_timer.is_met():
 			next_state = "display_state"
-		elif MANUAL_R.value == 1:
+		elif MANUAL_R.is_pressed:
 			user_selection = "manual"
 			next_state = "heat_setting_state"
-		elif PROGRAM_R.value == 1:
+		elif PROGRAM_R.is_pressed:
 			user_selection = "program"
 			next_state = "cook_time_state"
-		elif PROBE_R.value == 1: ######################### TAKE NOTES HERE, NOT SURE IF THIS IS WHAT HAPPENS #############################
+		elif PROBE_R.is_pressed: ######################### TAKE NOTES HERE, NOT SURE IF THIS IS WHAT HAPPENS #############################
 			start_temp = 160
-		elif UP_R.value == 1:
+		elif UP_R.is_pressed:
 			if start_temp < 180:
 				start_temp = start_temp + 5
-		elif DOWN_R.value == 1:
+		elif DOWN_R.is_pressed:
 			if start_temp > 140:
 				start_temp = start_temp - 5
 		else:
@@ -345,20 +345,20 @@ def display_state():
 	print("DISPLAY STATE")
 	while next_state == "display_state":
 		#control the next state
-		if ON_OFF.value == 1:
+		if ON_OFF.is_pressed:
 			next_state = "on_off_state"
 		##elif on_off_timer.is_met():
 		##	next_state = "on_off_state"
-		elif PROGRAM_R.value == 1:
+		elif PROGRAM_R.is_pressed:
 			next_state = "cook_time_state"
 			user_selection = "program"
-		elif MANUAL_R.value == 1:
+		elif MANUAL_R.is_pressed:
 			next_state = "heat_setting_state"
 			user_selection = "manual"
-		elif PROBE_R.value == 1 and ENTER_R.value == 1:
+		elif PROBE_R.is_pressed and ENTER_R.is_pressed:
             # temperature setting is changed to C, nothing to tell app
 			next_state = "display_state"
-		elif PROBE_R.value == 1:
+		elif PROBE_R.is_pressed:
 			next_state = "heat_setting_state"
 			user_selection = "probe"
 		else:
