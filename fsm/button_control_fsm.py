7a9e23ffc11af5316fc3da4abcecf0565be2f3fa
Mariah Doepker <m2doepker@gmail.com>
2019-03-14 09:22:11 -0400

    Idea didn't work, went back to a sleep statement.

--- fsm/button_control_fsm.py
+++ fsm/button_control_fsm.py
@@ -133,11 +133,11 @@ def cook_time_state():
 		elif UP_R.is_pressed:
 			if start_time < 23:
 				start_time = start_time + 1
-			UP_R.wait_for_release() #time.sleep(0.25)
+			time.sleep(0.20)
 		elif DOWN_R.is_pressed:
 			if start_time > 0:
 				start_time = start_time - 1
-			DOWN_R.wait_for_release() #time.sleep(0.25)
+			time.sleep(0.20)
 		else:
 			next_state = "cook_time_state"
 			
